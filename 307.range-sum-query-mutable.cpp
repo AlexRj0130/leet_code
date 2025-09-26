d3c766f33af8938fe2a75cda7b2729dad20b3aa3
zhangrunjie.aj <zhangrunjie.aj@bytedance.com>
2025-09-26 14:37:36 +0800

    update 307

--- 307.range-sum-query-mutable.cpp
+++ 307.range-sum-query-mutable.cpp
@@ -1,8 +1,5 @@
 #include <vector>
-#include <iostream>
-
 using namespace std;
-
 /*
  * @lc app=leetcode id=307 lang=cpp
  *
@@ -10,80 +7,96 @@ using namespace std;
  */
 
 // @lc code=start
+
+struct BinTreeNode {
+    int val;
+    int start;
+    int end;
+    BinTreeNode* left;
+    BinTreeNode* right;
+};
+
 class NumArray {
 public:
-    NumArray(vector<int>& nums) : mDataSize(nums.size()), mSeg(4 * nums.size(), 0) {
-        if (nums.size() <= 0) {
-            return;
-        }
-
-        buildSeg(nums, 0, nums.size() - 1, 0);
+    NumArray(vector<int>& nums) {
+        root = buildTree(nums, 0, nums.size() - 1);
     }
     
     void update(int index, int val) {
-        if (mDataSize <= 0 || index < 0 || index >= mDataSize) {
-            return;
-        }
-
-        updateSeg(0, mDataSize - 1, 0, index, val);
+        updateTree(index, val, root);
     }
     
     int sumRange(int left, int right) {
-        if (mDataSize <= 0) {
-            return 0;
-        }
-        return sumSeg(left, right, 0, mDataSize - 1, 0);
+        return sumTree(left, right, root); 
     }
 
 private:
-    void buildSeg(const vector<int>& nums, int left, int right, int pos) {
-        if (left >= right) {
-            mSeg[pos] = nums[left];
+    int sumTree(int left, int right, BinTreeNode *node) {
+        if (left > right || node == nullptr) {
+            return 0;
+        }
+
+        if (left <= node->start && right >= node->end) {
+            return node->val;
+        }
+
+        if (left > node->end || right < node->start) {
+            return 0;
+        }
+
+        return sumTree(left, right, node->left) + sumTree(left, right, node->right);
+    }
+
+    void updateTree(int index, int val, BinTreeNode * node) {
+        if (node == nullptr) {
             return;
         }
 
-        int mid = (left + right) / 2;
-        buildSeg(nums, left, mid, pos * 2 + 1);
-        buildSeg(nums, mid + 1, right, pos * 2 + 2);
-        mSeg[pos] = mSeg[pos * 2 + 1] + mSeg[pos * 2 + 2];
+        if (index < node->start || index > node->end) {
+            return;
+        }
+
+        if (index == node->start && index == node->end) {
+            node->val = val;
+            return;
+        }
+
+        updateTree(index, val, node->left);
+        updateTree(index, val, node->right);
+
+        node->val = 0;
+        if (node->left != nullptr) {
+            node->val += node->left->val;
+        }
+        if (node->right != nullptr) {
+            node->val += node->right->val;
+        }
         return;
     }
 
-    void updateSeg(int left, int right, int pos, int index, int val) {
-        if (index < left || index > right) {
-            return;
+    BinTreeNode * buildTree(const vector<int>& nums, int start, int end) {
+        if (start > end) {
+            return nullptr;
         }
 
-        if (left == right) {
-            if (index == left) {
-                mSeg[pos] = val;
-            }
-            return;
+        auto node = new BinTreeNode();
+        node->start = start;
+        node->end = end;
+
+        if (start == end) {
+            node->val = nums[start];
+            return node;
         }
 
-        int mid = (left + right) / 2;
-        updateSeg(left, mid, pos * 2 + 1, index, val);
-        updateSeg(mid + 1, right, pos * 2 + 2, index, val);
-        mSeg[pos] = mSeg[pos * 2 + 1] + mSeg[pos * 2 + 2];
-    }
-
-    int sumSeg(int queryLeft, int queryRight, int left, int right, int pos) {
-        if (queryLeft <= left && queryRight >= right) {
-            return mSeg[pos];
-        }
-
-        if (queryLeft > right || queryRight < left) {
-            return 0;
-        }
-
-        int mid = (left + right) / 2;
-        return sumSeg(queryLeft, queryRight, left, mid, pos * 2 + 1) + 
-            sumSeg(queryLeft, queryRight, mid + 1, right, pos * 2 + 2);
+        auto mid = (start + end) / 2;
+        node->left = buildTree(nums, start, mid);
+        node->right = buildTree(nums, mid + 1, end);
+        node->val = node->left->val + node->right->val;
+        return node;
     }
 
 private:
-    int mDataSize;
-    vector<int> mSeg;
+    BinTreeNode* root;
 };
 
 /**
@@ -93,15 +106,4 @@ private:
  * int param_2 = obj->sumRange(left,right);
  */
 // @lc code=end
-
-int main() {
-    auto vec = vector<int>{9, -8};
-    NumArray sol(vec);
-    sol.update(0, 3);
-    cout << "sumRange = " << sol.sumRange(1, 1) << endl;
-    cout << "sumRange = " << sol.sumRange(0, 1) << endl;
-    sol.update(1, -3);
-    cout << "sumRange = " << sol.sumRange(0, 1) << endl;
-    return 0;
-}
 
