388a9dc464138e0ea33c8a57ec561029e054044e
zhangrunjie.aj <zhangrunjie.aj@bytedance.com>
2025-09-26 16:21:14 +0800

    update 99

--- 99.recover-binary-search-tree.cpp
+++ 99.recover-binary-search-tree.cpp
@@ -1,10 +1,5 @@
-#include <utility>
-
-/*
- * @lc app=leetcode id=99 lang=cpp
- *
- * [99] Recover Binary Search Tree
- */
+#include <iostream>
+using namespace std;
 
 struct TreeNode {
     int val;
@@ -14,6 +9,12 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+
+/*
+ * @lc app=leetcode id=99 lang=cpp
+ *
+ * [99] Recover Binary Search Tree
+ */
 
 // @lc code=start
 /**
@@ -30,52 +31,63 @@ struct TreeNode {
 class Solution {
 public:
     void recoverTree(TreeNode* root) {
-        inOrder(root);
-        if (first != nullptr) {
-            if (second != nullptr) {
-                std::swap(second->val, firstNext->val);
-                return;
-            }
-            std::swap(first->val, firstNext->val);
-        }
+        mid(root); 
+        auto tmp = first->val;
+        first->val = second->val;
+        second->val = tmp;
     }
 
-private:
-    void inOrder(TreeNode* root) {
+    void midCout(TreeNode * root) {
         if (root == nullptr) {
             return;
         }
-
-        inOrder(root->left);
-        if (pre != nullptr) {
-            if (pre->val >= root->val) {
-                if (first != nullptr) {
-                    second = first;
-                    secondNext = firstNext;
-                } 
-                first = pre;
-                firstNext = root;
-            }
-        }
-        pre = root;
-        inOrder(root->right);
+        midCout(root->left);
+        std::cout << root->val << std::endl;
+        midCout(root->right);
     }
 
 private:
-    TreeNode *pre{nullptr}; 
-    TreeNode *first{nullptr};
-    TreeNode *firstNext{nullptr};
-    TreeNode *second{nullptr};
-    TreeNode *secondNext{nullptr};
+    void mid(TreeNode * node) {
+        if (node == nullptr) {
+            return;
+        }
+
+        mid(node->left);
+        if (pre == nullptr) {
+            pre = node;
+        } else {
+            if (pre->val > node->val) {
+                if (first == nullptr) {
+                    first = pre;
+                    second = node;
+                } else {
+                    second = node;
+                }
+            }
+            pre = node;
+        }
+        mid(node->right);
+    }
+
+private:
+    TreeNode * first = nullptr;
+    TreeNode * second = nullptr;
+    TreeNode * pre = nullptr;
 };
 // @lc code=end
 
 int main() {
-    auto node2 = TreeNode(2);
-    auto node3 = TreeNode(3, nullptr, &node2);
-    auto node1 = TreeNode(1, &node3, nullptr);
+    auto root = new TreeNode(3, 
+        new TreeNode(1), 
+        new TreeNode(4, 
+            new TreeNode(2), 
+            nullptr));
 
     Solution sol;
-    sol.recoverTree(&node1);
+    sol.midCout(root);
+    sol.recoverTree(root);
+    sol.midCout(root);
+
+    return 0;
 }
 
