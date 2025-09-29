3875a8bea3a287f40b31f9ed06b9cd2cd2b7fd21
zhangrunjie.aj <zhangrunjie.aj@bytedance.com>
2025-09-29 10:36:14 +0800

    add 102

--- 102.binary-tree-level-order-traversal.cpp
+++ 102.binary-tree-level-order-traversal.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+/*
+ * @lc app=leetcode id=102 lang=cpp
+ *
+ * [102] Binary Tree Level Order Traversal
+ */
+
+
 struct TreeNode {
     int val;
     TreeNode *left;
@@ -11,12 +18,6 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
-
-/*
- * @lc app=leetcode id=102 lang=cpp
- *
- * [102] Binary Tree Level Order Traversal
- */
 
 // @lc code=start
 /**
@@ -30,39 +31,72 @@ struct TreeNode {
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+struct QueueNode {
+    TreeNode * treeNode; 
+    int nodeLevel;
+
+    QueueNode(TreeNode * node, int level) : treeNode(node), nodeLevel(level) {}
+};
+
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> res; 
         if (root == nullptr) {
-            return res;
+            return {};
         }
 
-        queue<std::pair<TreeNode*, int>> q;
-        q.push({root, 1});
-        int curLevel = 0;
+        queue<QueueNode> queue;
+        queue.push({root, 0});
 
-        while (!q.empty()) {
-            auto pNode = q.front(); 
-            q.pop();
-
-            if (pNode.second != curLevel) {
-                res.push_back(vector<int>{});
-                curLevel = pNode.second;
+        vector<vector<int>> res;
+        while(!queue.empty()) {
+            vector<int> resItem;
+            auto item = queue.front();
+            queue.pop();
+            resItem.push_back(item.treeNode->val);
+            if (item.treeNode->left != nullptr) {
+                queue.push({item.treeNode->left, item.nodeLevel + 1});
             }
-            res[curLevel - 1].push_back(pNode.first->val);
-
-            if (pNode.first->left != nullptr) {
-                q.push({pNode.first->left, curLevel + 1});
+            if (item.treeNode->right != nullptr) {
+                queue.push({item.treeNode->right, item.nodeLevel + 1});
             }
 
-            if (pNode.first->right != nullptr) {
-                q.push({pNode.first->right, curLevel + 1});
+            while((!queue.empty()) && queue.front().nodeLevel== item.nodeLevel) {
+                auto sameItem = queue.front();
+                queue.pop();
+                resItem.push_back(sameItem.treeNode->val);
+                if (sameItem.treeNode->left != nullptr) {
+                    queue.push({sameItem.treeNode->left, sameItem.nodeLevel + 1});
+                }
+                if (sameItem.treeNode->right != nullptr) {
+                    queue.push({sameItem.treeNode->right, sameItem.nodeLevel + 1});
+                }
             }
+            res.push_back(resItem);
         }
 
         return res;
     }
 };
 // @lc code=end
+
+int main() {
+    auto root = new TreeNode(
+        1,
+        new TreeNode(
+            2,
+            new TreeNode(4),
+            nullptr
+        ),
+        new TreeNode(
+            3,
+            nullptr,
+            new TreeNode(5)
+        )
+    );
+
+    Solution sol;
+    sol.levelOrder(root);
+}
 
