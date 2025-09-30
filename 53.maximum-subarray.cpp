08ce7df6d07dd46f5a4c209dc178f9f6d0d405fe
zhangrunjie.aj <zhangrunjie.aj@bytedance.com>
2025-09-30 15:13:16 +0800

    update 53

--- 53.maximum-subarray.cpp
+++ 53.maximum-subarray.cpp
@@ -14,18 +14,19 @@ public:
     int maxSubArray(vector<int>& nums) {
         if (nums.size() <= 0) {
             return 0;
-        }
+        } 
 
-        if (nums.size() <= 1) {
+        if (nums.size() == 1) {
             return nums[0];
         }
 
+        vector<int> dp(nums.size(), 0);
+        dp[0] = nums[0];
+
         int res = nums[0];
-        int maxSum = nums[0];
-        
         for (int i = 1; i < nums.size(); ++i) {
-            maxSum = nums[i] > nums[i] + maxSum ? nums[i] : nums[i] + maxSum;
-            res = res > maxSum ? res : maxSum;
+            dp[i] = max(nums[i], nums[i] + dp[i-1]);
+            res = max(dp[i], res);
         }
 
         return res;
