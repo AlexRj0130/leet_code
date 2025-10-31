7106e639f7ffd05b020e9ed4acd8cf06bdf43f51
zhangrunjie.aj <zhangrunjie.aj@bytedance.com>
2025-10-31 14:26:30 +0800

    update

---
+++ 226.invert-binary-tree.cpp
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=226 lang=cpp
+ *
+ * [226] Invert Binary Tree
+ */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) {
+            return root;
+        } 
+
+        auto newLeft = invertTree(root->right);
+        auto newRight = invertTree(root->left);
+        root->left = newLeft;
+        root->right = newRight;
+        return root;
+    }
+};
+// @lc code=end
+
