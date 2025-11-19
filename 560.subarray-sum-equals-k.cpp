e66fce386e303daab46f8a5e7c39af320ce230a0
zhangrunjie.aj <zhangrunjie.aj@bytedance.com>
2025-11-19 13:21:57 +0800

    update 560, time long

--- 560.subarray-sum-equals-k.cpp
+++ 560.subarray-sum-equals-k.cpp
@@ -12,49 +12,19 @@ using namespace std;
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        if (nums.size() <= 0) {
-            return 0;
-        } 
-
-        initDP(nums);
-        iterateDP(nums); 
-        auto res = countDP(nums, k);
-        return res;
-    }
-private:
-    void initDP(const vector<int>& nums) {
-        if (nums.size() <= 0) {
-            return;
-        }
-
-        auto numSize = nums.size();
-        dp = vector<vector<int>>(numSize, vector<int>(numSize, 0));
-        for (int i = 0; i < numSize; ++i) {
-            dp[i][i] = nums[i];
-        }
-    }
-
-    void iterateDP(const vector<int>& nums) {
-        for (int i = 0; i < nums.size(); ++i) {
-            for (int j = i + 1; j < nums.size(); ++j) {
-                dp[i][j] = dp[i][j-1] + nums[j]; 
-            }
-        }
-    }
-
-    int countDP(const vector<int>& nums, int target) {
         int res = 0;
         for (int i = 0; i < nums.size(); ++i) {
+            int sum = 0;
             for (int j = i; j < nums.size(); ++j) {
-                if (dp[i][j] == target) {
+                sum += nums[j];
+                if (sum == k) {
                     ++res;
                 }
             }
         }
+
         return res;
     }
-private:
-    vector<vector<int>> dp;
 };
 // @lc code=end
 
