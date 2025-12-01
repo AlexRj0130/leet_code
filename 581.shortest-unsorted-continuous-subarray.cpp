dcda0d70cde8332b99552057e22cb4cb686cb134
zhangrunjie.aj <zhangrunjie.aj@bytedance.com>
2025-12-01 16:01:31 +0800

    update 581

--- 581.shortest-unsorted-continuous-subarray.cpp
+++ 581.shortest-unsorted-continuous-subarray.cpp
@@ -12,62 +12,25 @@ using namespace std;
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
-        if (nums.size() <= 1) {
-            return 0;
-        } 
+        int size = nums.size();
+        int minVal = nums[size - 1], maxVal = nums[0];
+        int sta = -1, end = -2;
 
-        int minRevNum = 0, minRevNumIndex = 0;
-        int maxRevNum = 0, maxRevNumIndex = 0;
-        bool findMinRevNum = false, findMaxRevNum = false;
-
-        for (int i = 1; i < nums.size(); ++i) {
-            if (nums[i] < nums[i - 1]) {
-                if (findMinRevNum == false) {
-                    minRevNum = nums[i];
-                    minRevNumIndex = i;
-                    findMinRevNum = true;
-                } else {
-                    if (nums[i] < minRevNum) {
-                        minRevNum = nums[i];
-                        minRevNumIndex = i;
-                    }
-                }
-
-                if (findMaxRevNum == false) {
-                    maxRevNum = nums[i - 1];
-                    maxRevNumIndex = i - 1;
-                    findMaxRevNum = true;
-                } else {
-                    if (nums[i - 1] > maxRevNum) {
-                        maxRevNum = nums[i - 1];
-                        maxRevNumIndex = i - 1;
-                    }
-                }
+        for (int i = 1; i < size; ++i) {
+            maxVal = max(maxVal, nums[i]);
+            if (nums[i] < maxVal) {
+                end = i;
             }
         }
 
-        if (findMinRevNum == false || findMaxRevNum == false) {
-            return 0;
-        }
-        
-        int minTargetIndex = 0, maxTargetIndex = nums.size() - 1;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] <= minRevNum) {
-                minTargetIndex = i + 1;
-                continue;
+        for (int i = size - 2; i >= 0; --i) {
+            minVal = min(minVal, nums[i]);
+            if (nums[i] > minVal) {
+                sta = i;
             }
-            break;
         }
 
-        for (int i = nums.size() - 1; i >= 0; --i) {
-            if (nums[i] >= maxRevNum) {
-                maxTargetIndex = i - 1;
-                continue;
-            }
-            break;
-        }
-
-        return maxTargetIndex - minTargetIndex + 1;
+        return end - sta + 1;
     }
 };
 // @lc code=end
