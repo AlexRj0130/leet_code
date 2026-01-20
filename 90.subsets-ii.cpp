1cf1981804e70436dd724d6f708b5471424964ec
zhangrunjie.aj <zhangrunjie.aj@bytedance.com>
2026-01-20 11:53:33 +0800

    update 90

--- 90.subsets-ii.cpp
+++ 90.subsets-ii.cpp
@@ -20,35 +20,30 @@ public:
 
         sort(nums.begin(), nums.end());
         vector<int> trace;
-        unordered_map<string, bool> existMap;
-        dfs(res, nums, trace, 0, existMap); 
+        dfs(res, nums, trace, 0); 
 
         return res;
     }
 private:
-    void dfs(vector<vector<int>> &res, const vector<int>& nums, vector<int>& trace, int index, unordered_map<string, bool> &existMap) {
+    void dfs(vector<vector<int>> &res, const vector<int>& nums, vector<int>& trace, int index) {
         if (index >= nums.size()) {
             return;
         }
 
+        int preVal = 1000;
         for (int i = index; i < nums.size(); ++i) {
-            trace.push_back(nums[i]);
-            auto traceKey = vector2string(trace);
-            if (existMap.find(traceKey) == existMap.end()) {
-                res.push_back(trace);
-                existMap[traceKey] = true;
+            auto curVal = nums[i];
+            if (curVal == preVal) {
+                continue;
             }
-            dfs(res, nums, trace, i + 1, existMap);
+
+            trace.push_back(curVal);
+            res.push_back(trace);
+            preVal = curVal;
+
+            dfs(res, nums, trace, i + 1);
             trace.pop_back();
         }
-    }
-
-    string vector2string(const vector<int> &vec) {
-        string res = "";
-        for (auto &item : vec) {
-            res = res + to_string(item) + ",";
-        }
-        return res;
     }
 };
 // @lc code=end
