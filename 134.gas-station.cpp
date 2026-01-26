921d66d0ff7589f129119b404b228626b28ae01f
zhangrunjie.aj <zhangrunjie.aj@bytedance.com>
2026-01-26 11:54:42 +0800

    update 134

--- 134.gas-station.cpp
+++ 134.gas-station.cpp
@@ -14,23 +14,21 @@ using namespace std;
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int total_gas = 0, total_cost = 0; // 全局总油量和总耗油量
-        int current_tank = 0, start = 0;   // 当前油箱油量、候选起点
-        
+        int totalGas = 0, totalCost = 0;
+        int totalTank = 0, res = 0;
+
         for (int i = 0; i < gas.size(); ++i) {
-            total_gas += gas[i];
-            total_cost += cost[i];
-            current_tank += gas[i] - cost[i]; // 累计当前起点的剩余油量
-            
-            // 若当前起点无法到达下一个加油站，更新起点为下一个
-            if (current_tank < 0) {
-                start = i + 1;
-                current_tank = 0;
+            totalGas += gas[i];
+            totalCost += cost[i];
+            totalTank += gas[i] - cost[i];
+
+            if (totalTank < 0) {
+                res = i + 1;
+                totalTank = 0;
             }
         }
-        
-        // 总油量足够时返回起点，否则返回-1
-        return total_gas >= total_cost ? start : -1;
+
+        return totalGas >= totalCost ? res : -1;
     }
 };
 // @lc code=end
