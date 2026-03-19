e273fb11f0a40612790bdb884765540212c7f999
arjen <zrj0130@163.com>
2026-03-19 19:24:32 +0800

    update 241

--- 241.different-ways-to-add-parentheses.cpp
+++ 241.different-ways-to-add-parentheses.cpp
@@ -13,111 +13,74 @@ using namespace std;
 class Solution {
 public:
     vector<int> diffWaysToCompute(string expression) {
-        vector<string> splitExpr = splitExpression(expression); 
-
-        vector<vector<string>> postOrderExpr; 
-        dfsToPostOrder(splitExpr, 0, {}, {}, postOrderExpr);
+        auto expr = removeSpace(expression);
 
         vector<int> res;
-        for (const auto &expr : postOrderExpr) {
-            auto resItem = calculatePostOrder(expr);
-            res.push_back(resItem);
-        }
+        dfs(expr, res);
         return res;
     }
 private:
-    int calculatePostOrder(const vector<string> &expr) {
-        vector<string> vStack;
-        for (const auto &item : expr) {
-            if (!isOperator(item)) {
-                vStack.push_back(item);
-            } else {
-                auto b = vStack.back();
-                vStack.pop_back();
-                auto a = vStack.back();
-                vStack.pop_back();
-                vStack.push_back(calculateHelp(a, b, item));
-            }
-        }
-        return stoi(vStack[0]);
-    }
-
-    string calculateHelp(const string &a, const string &b, const string &op) {
-        int aInt = stoi(a);
-        int bInt = stoi(b);
-        int res = 0;
-        if (op == "+") {
-            res = aInt + bInt; 
-        } else if (op == "-") {
-            res = aInt - bInt;
-        } else if (op == "*") {
-            res = aInt * bInt;
-        } else {
-            res = 0;
-        }
-        return to_string(res);
-    }
-
-    void dfsToPostOrder(const vector<string> &expr, int depth, vector<string> trace, vector<string> opStack, vector<vector<string>> &res) {
-        if (depth >= expr.size()) {
-            while (!opStack.empty()) {
-                trace.push_back(opStack.back());
-                opStack.pop_back();
-            }
-            res.push_back(trace);
+    void dfs(const string &expression, vector<int> &res) {
+        if (expression.size() <= 0) {
             return;
         }
 
-        const auto &item = expr[depth];
-        if (!isOperator(item)) {
-            trace.push_back(item);
-            dfsToPostOrder(expr, depth + 1, trace, opStack, res);
-        } else {
-            int opStackSize = opStack.size();
-            for (int i = 0; i <= opStackSize; ++i) {
-                opStack.push_back(item);
-                dfsToPostOrder(expr, depth + 1, trace, opStack, res);
-                opStack.pop_back();
+        bool noOperator = true;
+        for (int i = 0; i < expression.size(); ++i) {
+            const auto ch = expression[i];
+            if (isOperator(ch)) {
+                noOperator = false;
 
-                if (!opStack.empty()) {
-                    trace.push_back(opStack.back());
-                    opStack.pop_back();
+                string leftExpr = expression.substr(0, i);
+                vector<int> leftRes;
+                dfs(leftExpr, leftRes);
+
+                string rightExpr = expression.substr(i + 1, expression.size() - i - 1);
+                vector<int> rightRes;
+                dfs(rightExpr, rightRes);
+
+                for (auto a : leftRes) {
+                    for (auto b : rightRes) {
+                        auto tmp = calculateHelp(a, b, ch);
+                        res.push_back(tmp);
+                    }
                 }
             }
         }
+
+        if (!noOperator) {
+            return;
+        }
+
+        res.push_back(stoi(expression));
     }
 
-    bool isOperator(const string &val) {
-        return val == "+" || val == "-" || val == "*";
+    int calculateHelp(int a, int b, char op) {
+        switch (op) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+        }
+
+        return 0;
     }
 
-    vector<string> splitExpression(const string &expr) {
-        vector<string> res;
-        
-        string numTmp = "";
-        for (auto item : expr) {
+    string removeSpace(const string &expression) {
+        string res;
+        for (const auto &item : expression) {
             if (isspace(item)) {
-                if (numTmp.size() > 0) {
-                    res.push_back(numTmp);
-                    numTmp = "";
-                }
                 continue;
-            } else if (isdigit(item)) {
-                numTmp += item;
-            } else if (item == '+' || item == '-' || item == '*') {
-                if (numTmp.size() > 0) {
-                    res.push_back(numTmp);
-                    numTmp = "";
-                } 
-                res.push_back(string("") + item);
             }
+            res += item;
         }
-        if (numTmp.size() > 0) {
-            res.push_back(numTmp);
-            numTmp = "";
-        }
-
         return res;
+    }
+
+    bool isOperator(const char ch) {
+        return ch == '+' || ch == '-' || ch == '*';
     }
 };
 // @lc code=end
