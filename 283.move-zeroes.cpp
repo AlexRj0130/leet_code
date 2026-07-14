2dd52bdafafd8d09d6013d1ae474067348ac08f1
arjen <zrj0130@163.com>
2026-07-14 14:20:04 +0800

    update 283

--- 283.move-zeroes.cpp
+++ 283.move-zeroes.cpp
@@ -12,31 +12,52 @@ using namespace std;
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int firstZeroIndex = -1;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] != 0) {
-                continue;
-            }
-            firstZeroIndex = i;
-            break;
-        }
-        if (firstZeroIndex == -1) {
+        // 思路：
+        //  先找到第一个 0 的位置 zeroIndex
+        //  然后，从第一个 0 的位置 zeroIndex 开始，向后找到第一个非 0 的位置 notZeroIndex
+        //  交换 zeroIndex 和 notZeroIndex 对应的数值
+        //  然后从 zeroIndex 开始（不含）找到下一个 0 的位置 zeroIndex
+        //  重复上述过程
+
+        int zeroIndex = findNextZeroIndex(nums, 0);
+        if (zeroIndex < 0) {
             return;
         }
 
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] == 0) {
-                continue;
-            }
-            if (firstZeroIndex >= i) {
-                continue;
-            }
-            nums[firstZeroIndex] = nums[i];
-            nums[i] = 0;
-            ++firstZeroIndex;
+        int notZeroIndex = -1;
+        while (notZeroIndex = findNextNotZeroIndex(nums, zeroIndex), notZeroIndex > 0) {
+            swap(nums[zeroIndex], nums[notZeroIndex]);
+            zeroIndex = findNextZeroIndex(nums, zeroIndex + 1);
         }
     }
 private:
+    int findNextZeroIndex(const vector<int>& nums, int sta) {
+        if (sta < 0 || sta >= nums.size()) {
+            return -1;
+        }
+
+        for (int i = sta; i < nums.size(); ++i) {
+            if (nums[i] == 0) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    int findNextNotZeroIndex(const vector<int>& nums, int sta) {
+        if (sta < 0 || sta >= nums.size()) {
+            return -1;
+        }
+
+        for (int i = sta; i < nums.size(); ++i) {
+            if (nums[i] != 0) {
+                return i;
+            }
+        }
+
+        return -1;
+    } 
 };
 // @lc code=end
 
