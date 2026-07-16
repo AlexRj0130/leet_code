053e829df67c1094e00ea8780dd2d58847529eb7
arjen <zrj0130@163.com>
2026-07-16 12:17:35 +0800

    update 136

--- 136.single-number.cpp
+++ 136.single-number.cpp
@@ -12,9 +12,9 @@ using namespace std;
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int res = 0; 
-        for (int i = 0; i < nums.size(); ++i) {
-            res = res ^ nums[i];
+        int res = 0;
+        for (auto num : nums) {
+            res ^= num;
         }
         return res;
     }
